aad9b41860e0eae4a6685ff4a710f57373cb3481
Alewesley-Sousa <alewesley1234@gmail.com>
2025-10-03 16:28:59 -0300

    faze de testes para esse codigo que tem que retornar os usuarios.

--- app/models/model.php
+++ app/models/model.php
@@ -1,8 +1,12 @@
 <?php
-
+include_once('conexao.php')
 // vamos criar os comandos primodiais sem confirmação de nada
 
+$sql = 'SELECT * FROM usuarios';
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
 
+$usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
+print_r($usuarios);
 ?>
