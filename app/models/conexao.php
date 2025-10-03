e08fc1de2ef90a68b1bff1b905a761df411821c2
= <=>
2025-10-03 12:21:37 -0300

    conexão com o banco de dados estabelecida.

--- app/models/conexao.php
+++ app/models/conexao.php
@@ -3,3 +3,20 @@ define('DB_HOST', 'localhost');
 define('DB_NAME','acesso_autenticacao');
 define('DB_USER','root');
 define('DB_PASSWORD','');
+
+try {
+    $dns = "mysql:host" . DB_HOST .";dbname=". DB_NAME .";chasert=utf8mb4";
+    
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    $pdo = new PDO($dns, DB_USER, DB_PASSWORD, $options);
+} catch (PDOException $e) {
+    die("eero na conexão com o banco de dados: " . $e->getMessage());
+}
+?>
