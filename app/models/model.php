cf122cb446fc86dee87517542462aa3d55c08a37
Alewesley-Sousa <alewesley1234@gmail.com>
2025-10-16 20:35:31 -0300

    model quase todo finalizado, depois deve-se criar o controle dessas informações

--- app/models/model.php
+++ app/models/model.php
@@ -1,6 +1,7 @@
 <?php 
 // conecta com o arquivo que tem a conexão com o banco de dados que esta na variavel $pdo
 require_once 'conexao.php';
+session_start();
 class Model {
     private $pdo;
     public function __construct($pdo) {
@@ -20,14 +21,12 @@ class Model {
         return $stmt->fetch(PDO::FETCH_ASSOC);
         
     }
-}
 
-$model = new Model($pdo);
-$usuario = $model->get(1);
-
-if ($usuario) {
-    echo 'existe um usuario com este id';
-} else {
-    echo 'não tem um usuario com este id';
+    public function adicionar($usuario) {
+        $sql = "INSERT INTO usuario(usuario, senha, nome, email, nivel_acesso) VALUE(?, ?, ?, ?, ?)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$usuario['usuario'], $usuario['senha'], $usuario['nome'], $usuario['email'], $usuario['acesso']]);
+        $_SESSION['sucesso'] = 'usuario criado com sucesso!';
+    }
 }
 ?>
