50d65ebe5b44e31c013281e9927ae9accdd26f20
= <=>
2025-10-29 08:18:22 -0300

    criando o controller do sistema

---
+++ app/controller/modelController.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../models/conexao.php';
+require_once '../models/model.php';
+
+class modelController {
+    public function criar($usuario)
+    {
+
+    }
+
+    public function validarCampos($usuario) {
+        
+    }
+}
